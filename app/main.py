676a8f8e05b29329f9c355121abe005e9a3e5e7c
Tharindu Sathsara <tharindusathsarahome@gmail.com>
2024-06-25 19:51:52 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,9 +33,9 @@ app.add_middleware(
 app.include_router(handle_facebook.router, prefix="/social-media/handle-facebook", tags=["facebook"])
 app.include_router(handle_platform_insights.router, prefix="/social-media/platform-insights", tags=["platform-insights"])
 app.include_router(handle_campaign_analysis.router, prefix="/social-media/campaign-analysis", tags=["campaign-analysis"])
+app.include_router(handle_products_keywords.router, prefix="/social-media/products-keywords", tags=["products-keywords"])
 app.include_router(handle_settings.router, prefix="/social-media/settings", tags=["settings"])
 app.include_router(handle_utils.router, prefix="/social-media/utils", tags=["utils"])
-app.include_router(handle_products_keywords.router, prefix="/social-media/products-keywords", tags=["products-keywords"])
 
 # Events
 app.add_event_handler("startup", start_scheduler)
